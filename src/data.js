09168839e47d01752dcba9ace27b48ddc5beb31e
SreelekhaKadireddy <ksreelekha049@gmail.com>
2023-11-06 14:27:04 +0530

    portfolio website using react

---
+++ src/data.js
@@ -0,0 +1,47 @@
+export const projects =[
+    {
+        title: "React To-do app",
+        subtitle: "To-Do list web application",
+        description:
+          "React project to develop a to-do list web application.",
+        image:"./todo.png",
+        link: "https://github.com/SreelekhaKadireddy/React-To-Do-App",
+      },
+      {
+        title: "Resume builder application",
+        subtitle: "Web development project",
+        description:
+          "Resume builder application is developed using HTML, BOOTSTRAP, JAVASCRIPT",
+        image: "./resume.png",
+        link: "https://github.com/SreelekhaKadireddy/resume-builder-website",
+      },
+      {
+        title: "Quiz app",
+        subtitle: "Front end quiz application",
+        description:
+          "Quiz application is a frontend web development project developed using HTML,CSS,JS",
+        image: "./quiz.png",
+        link: "https://github.com/SreelekhaKadireddy/Quiz-app",
+      },
+      {
+        title: "Digital Calculator",
+        subtitle: "Calculator to perform mathematical operations",
+        description:
+          "Digital calculator is a web application to perform simple mathematical applications",
+        image: "./calculator.png",
+        link: "https://github.com/SreelekhaKadireddy/calculator",
+      },
+    ];
+export const skills =[
+    "HTML",
+    "CSS",
+    "JavaScript",
+    "Bootstrap",
+    "Nodejs",
+    "Express",
+    "MongoDB",
+    "MySQL",
+    "Python",
+    "React JS",
+    "FastAPI"
+    ];
